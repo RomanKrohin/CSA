997edd6617965ba7649742d955b400961728652d
Roman <romankrohin126@gmail.com>
2024-05-09 17:57:28 +0300

    Logging

--- S-P-ASM/Start.py
+++
@@ -1,7 +0,0 @@
-from translate.Parser import run
-from translate.Translator import to_binary, from_binary
-from cpu.loader_to_cpu import load_binary_code
-
-if __name__ == "__main__":
-    to_binary (run("examples/test.SPASM"), "output/test.bin")
-    load_binary_code("output/test.bin")
